d5ef07a8ff2344399663cb6d3a44da109fd33a98
seyko <seyko2@gmail.com>
2023-05-22 13:24:17 +0800

    fix for the #include_next

--- tccpp.c
+++ tccpp.c
@@ -1595,7 +1595,6 @@ redo:
         for (i = -2; i < n; ++i) {
             char buf1[sizeof file->filename];
             CachedInclude *e;
-            BufferedFile **f;
             const char *path;
 
             if (i == -2) {
@@ -1625,13 +1624,12 @@ redo:
             pstrcat(buf1, sizeof(buf1), buf);
 
             if (tok == TOK_INCLUDE_NEXT)
-                for (f = s1->include_stack_ptr; f >= s1->include_stack; --f)
-                    if (0 == PATHCMP((*f)->filename, buf1)) {
+                if (0 == PATHCMP(file->filename, buf1)) {
 #ifdef INC_DEBUG
-                        printf("%s: #include_next skipping %s\n", file->filename, buf1);
+                    printf("%s: #include_next skipping %s\n", file->filename, buf1);
 #endif
-                        goto include_trynext;
-                    }
+                    goto include_trynext;
+                }
 
             e = search_cached_include(s1, buf1);
             if (e && (define_find(e->ifndef_macro) || e->ifndef_macro == TOK_once)) {
