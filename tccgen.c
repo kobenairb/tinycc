e421b19a04cc02f0cf0e74e18ca0a5d6c4487d13
grischka <grischka>
2023-05-22 12:37:43 +0800

    avoid "decl after statement" please

--- tccgen.c
+++ tccgen.c
@@ -4006,9 +4006,10 @@ tok_next:
             vtop->r2 = ret.r2;
             /* handle packed struct return */
             if (((s->type.t & VT_BTYPE) == VT_STRUCT) && !sret) {
+                int addr;
                 size = type_size(&s->type, &align);
                 loc = (loc - size) & -align;
-                int addr = loc;
+                addr = loc;
                 vset(&ret.type, VT_LOCAL | VT_LVAL, addr);
                 vswap();
                 vstore();
@@ -4497,6 +4498,7 @@ static void block(int *bsym, int *csym, int *case_sym, int *def_sym, int case_re
         gsym_addr(b, d);
     } else if (tok == '{') {
         Sym *llabel;
+        int block_vla_sp_loc, *saved_vla_sp_loc, saved_vla_flags;
 
         next();
         /* record local declaration stack position */
@@ -4507,12 +4509,11 @@ static void block(int *bsym, int *csym, int *case_sym, int *def_sym, int case_re
         llabel = local_label_stack;
 
         /* save VLA state */
-        int block_vla_sp_loc = *vla_sp_loc;
-        int *saved_vla_sp_loc = vla_sp_loc;
+        block_vla_sp_loc = *(saved_vla_sp_loc = vla_sp_loc);
         if (saved_vla_sp_loc != &vla_sp_root_loc)
             vla_sp_loc = &block_vla_sp_loc;
 
-        int saved_vla_flags = vla_flags;
+        saved_vla_flags = vla_flags;
         vla_flags |= VLA_NEED_NEW_FRAME;
 
         /* handle local labels declarations */
