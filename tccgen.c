857914f541527cfd010d4771345d71f5ab0bce60
grischka <grischka>
2023-05-22 12:38:06 +0800

    tccgen: fix crash with undeclared struct ... as in:     #include<stdio.h>     int main()     {         struct asdasd x;         printf("%d\n", sizeof(x));     } This fixes commit 17571298f30bf204fafe9cf1aca5258d2d087d63

--- tccgen.c
+++ tccgen.c
@@ -2799,6 +2799,7 @@ static void struct_decl(CType *type, int u)
         v = anon_sym++;
     }
     type1.t = a;
+    type1.ref = NULL;
     /* we put an undefined size for struct/union */
     s = sym_push(v | SYM_STRUCT, &type1, 0, -1);
     s->r = 0; /* default alignment is zero as gcc */
@@ -5320,12 +5321,13 @@ static void decl_initializer_alloc(
 
     flexible_array = NULL;
     if ((type->t & VT_BTYPE) == VT_STRUCT) {
-        Sym *field;
-        field = type->ref;
-        while (field && field->next)
-            field = field->next;
-        if (field->type.t & VT_ARRAY && field->type.ref->c < 0)
-            flexible_array = field;
+        Sym *field = type->ref->next;
+        if (field) {
+            while (field->next)
+                field = field->next;
+            if (field->type.t & VT_ARRAY && field->type.ref->c < 0)
+                flexible_array = field;
+        }
     }
 
     size = type_size(type, &align);
