578e25798402f9cdf78f8aa6cd1147defb21e716
Michael Matz <matz@suse.de>
2023-05-22 14:30:37 +0800

    opt: Don't emit inline functions from dead code

--- tests/tcctest.c
+++ tests/tcctest.c
@@ -1169,6 +1169,11 @@ void bool_test()
 extern int undefined_function(void);
 extern int defined_function(void);
 
+static inline void refer_to_undefined(void)
+{
+    undefined_function();
+}
+
 void optimize_out(void)
 {
     int i = 0 ? undefined_function() : defined_function();
@@ -1264,6 +1269,9 @@ void optimize_out(void)
         ;
     else
         undefined_function();
+
+    if (defined_function() && 0)
+        refer_to_undefined();
 
     if (1)
         return;
