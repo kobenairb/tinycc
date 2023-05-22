9886507d29dfe9950f6736050de1f3b0af585c48
mob <mob>
2023-05-22 12:14:10 +0800

    Multiple fixes for 64 bit sections

--- tccrun.c
+++ tccrun.c
@@ -36,7 +36,7 @@ static int rt_get_caller_pc(uplong *paddr, ucontext_t *uc, int level);
 static void rt_error(ucontext_t *uc, const char *fmt, ...);
 static int tcc_relocate_ex(TCCState *s1, void *ptr);
 
-#ifdef _WIN64
+#if defined TCC_TARGET_X86_64 && defined TCC_TARGET_PE
 static void win64_add_function_table(TCCState *s1);
 #endif
 
@@ -192,7 +192,7 @@ static int tcc_relocate_ex(TCCState *s1, void *ptr)
     set_pages_executable(s1->runtime_plt_and_got, s1->runtime_plt_and_got_offset);
 #endif
 
-#ifdef _WIN64
+#if defined TCC_TARGET_X86_64 && defined TCC_TARGET_PE
     win64_add_function_table(s1);
 #endif
     return 0;
@@ -614,13 +614,15 @@ static void set_exception_handler(void)
     SetUnhandledExceptionFilter(cpu_exception_handler);
 }
 
-#ifdef _WIN64
+#ifdef TCC_TARGET_PE
+#ifdef TCC_TARGET_X86_64
 static void win64_add_function_table(TCCState *s1)
 {
     RtlAddFunctionTable((RUNTIME_FUNCTION *) (uplong) s1->uw_pdata->sh_addr,
                         s1->uw_pdata->data_offset / sizeof(RUNTIME_FUNCTION),
                         (uplong) text_section->sh_addr);
 }
+#endif
 #endif
 
 /* return the PC at frame level 'level'. Return non zero if not found */
