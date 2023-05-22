e5f509376559daa7386feb9e3bbfdfa6788aafac
Michael Matz <matz@suse.de>
2023-05-22 14:52:17 +0800

    Tidy decl_designator

--- tccgen.c
+++ tccgen.c
@@ -5958,7 +5958,6 @@ static void decl_designator(
 {
     Sym *s, *f;
     int notfirst, index, index_last, align, l, nb_elems, elem_size;
-    CType type1;
 
     notfirst = 0;
     elem_size = 0;
@@ -6008,10 +6007,7 @@ static void decl_designator(
                 expect("field");
             if (!notfirst)
                 *cur_field = f;
-            /* XXX: fix this mess by using explicit storage field */
-            type1 = f->type;
-            type1.t |= (type->t & ~VT_TYPE);
-            type = &type1;
+            type = &f->type;
             c += f->c;
         }
         notfirst = 1;
@@ -6036,10 +6032,7 @@ static void decl_designator(
                 *cur_field = f = f->next;
             if (!f)
                 tcc_error("too many field init");
-            /* XXX: fix this mess by using explicit storage field */
-            type1 = f->type;
-            type1.t |= (type->t & ~VT_TYPE);
-            type = &type1;
+            type = &f->type;
             c += f->c;
         }
     }
