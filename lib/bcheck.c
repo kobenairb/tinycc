9f2e46405fee620350137d876884d8bb3115729e
seyko <seyko2@gmail.com>
2023-05-22 13:10:56 +0800

    a small revers for bcheck.o changes (d80593bc4d43)

--- lib/bcheck.c
+++ lib/bcheck.c
@@ -204,7 +204,7 @@ void *FASTCALL __bound_ptr_add(void *p, size_t offset)
             addr = (size_t) p - e->start;                                 \
         }                                                                 \
         addr += offset + dsize;                                           \
-        if (addr >= e->size)                                              \
+        if (addr > e->size)                                               \
             return INVALID_POINTER; /* return an invalid pointer */       \
         return p + offset;                                                \
     }
