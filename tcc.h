7c5dd629477af527aa8f8defa69d854e5d47b8a2
Thomas Preud'homme <robotux@celest.fr>
2023-05-22 12:40:56 +0800

    Don't enable bound check if libgcc is used

--- tcc.h
+++ tcc.h
@@ -128,7 +128,7 @@ typedef UINT_PTR uintptr_t;
 #endif
 
 #if !defined(TCC_UCLIBC) && !defined(TCC_TARGET_ARM) && !defined(TCC_TARGET_C67) \
-    && !defined(TCC_TARGET_X86_64)
+    && !defined(TCC_TARGET_X86_64) && !defined(CONFIG_USE_LIBGCC)
 #define CONFIG_TCC_BCHECK /* enable bound checking code */
 #endif
 
