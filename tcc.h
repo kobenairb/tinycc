9463a53ede899eb9cbb4889429532bc1f2094485
Joe Soroka <gits@joesoroka.com>
2023-05-22 12:09:47 +0800

    revert complicated & broken flexible array member handling

--- tcc.h
+++ tcc.h
@@ -283,8 +283,7 @@ typedef struct AttributeDef
 {
     unsigned func_call : 3, /* calling convention (0..5), see below */
         aligned : 5,        /* alignement (0..16) */
-        packed : 1, func_export : 1, func_import : 1, func_args : 5, mode : 4, weak : 1, resize : 1,
-        fill : 10;
+        packed : 1, func_export : 1, func_import : 1, func_args : 5, mode : 4, weak : 1, fill : 11;
     struct Section *section;
     int alias_target; /* token */
 } AttributeDef;
@@ -296,7 +295,6 @@ typedef struct AttributeDef
 #define FUNC_ARGS(r) (((AttributeDef *) &(r))->func_args)
 #define FUNC_ALIGN(r) (((AttributeDef *) &(r))->aligned)
 #define FUNC_PACKED(r) (((AttributeDef *) &(r))->packed)
-#define ARRAY_RESIZE(r) (((AttributeDef *) &(r))->resize)
 #define ATTR_MODE(r) (((AttributeDef *) &(r))->mode)
 #define INT_ATTR(ad) (*(int *) (ad))
 
--- tests/tcctest.c
+++ tests/tcctest.c
@@ -1386,6 +1386,7 @@ void init_test(void)
            cix[0].b[2].a,
            cix[0].b[2].b);
     printf("cix2: %d %d\n", cix21.b[2], cix22.b[5]);
+    printf("sizeof cix21 %d, sizeof cix22 %d\n", sizeof cix21, sizeof cix22);
 }
 
 void switch_test()
