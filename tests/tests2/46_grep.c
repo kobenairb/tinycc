1a5d55c45bb11351ae945af3d9f726013c15d7ee
minux <minux.ma@gmail.com>
2023-05-22 12:51:40 +0800

    tests2: fix and enable 46_grep test.

--- tests/tests2/46_grep.c
+++ tests/tests2/46_grep.c
@@ -15,6 +15,7 @@
  * privileges were granted by DECUS.
  */
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
  * grep
@@ -25,52 +26,49 @@
  * See below for more information.
  */
 
-#if 0
-char    *documentation[] = {
-   "grep searches a file for a given pattern.  Execute by",
-   "   grep [flags] regular_expression file_list\n",
-   "Flags are single characters preceded by '-':",
-   "   -c      Only a count of matching lines is printed",
-   "   -f      Print file name for matching lines switch, see below",
-   "   -n      Each line is preceded by its line number",
-   "   -v      Only print non-matching lines\n",
-   "The file_list is a list of files (wildcards are acceptable on RSX modes).",
-   "\nThe file name is normally printed if there is a file given.",
-   "The -f flag reverses this action (print name no file, not if more).\n",
-   0 };
+char *documentation[]
+    = {"grep searches a file for a given pattern.  Execute by",
+       "   grep [flags] regular_expression file_list\n",
+       "Flags are single characters preceded by '-':",
+       "   -c      Only a count of matching lines is printed",
+       "   -f      Print file name for matching lines switch, see below",
+       "   -n      Each line is preceded by its line number",
+       "   -v      Only print non-matching lines\n",
+       "The file_list is a list of files (wildcards are acceptable on RSX modes).",
+       "\nThe file name is normally printed if there is a file given.",
+       "The -f flag reverses this action (print name no file, not if more).\n",
+       0};
 
-char    *patdoc[] = {
-   "The regular_expression defines the pattern to search for.  Upper- and",
-   "lower-case are always ignored.  Blank lines never match.  The expression",
-   "should be quoted to prevent file-name translation.",
-   "x      An ordinary character (not mentioned below) matches that character.",
-   "'\\'    The backslash quotes any character.  \"\\$\" matches a dollar-sign.",
-   "'^'    A circumflex at the beginning of an expression matches the",
-   "       beginning of a line.",
-   "'$'    A dollar-sign at the end of an expression matches the end of a line.",
-   "'.'    A period matches any character except \"new-line\".",
-   "':a'   A colon matches a class of characters described by the following",
-   "':d'     character.  \":a\" matches any alphabetic, \":d\" matches digits,",
-   "':n'     \":n\" matches alphanumerics, \": \" matches spaces, tabs, and",
-   "': '     other control characters, such as new-line.",
-   "'*'    An expression followed by an asterisk matches zero or more",
-   "       occurrences of that expression: \"fo*\" matches \"f\", \"fo\"",
-   "       \"foo\", etc.",
-   "'+'    An expression followed by a plus sign matches one or more",
-   "       occurrences of that expression: \"fo+\" matches \"fo\", etc.",
-   "'-'    An expression followed by a minus sign optionally matches",
-   "       the expression.",
-   "'[]'   A string enclosed in square brackets matches any character in",
-   "       that string, but no others.  If the first character in the",
-   "       string is a circumflex, the expression matches any character",
-   "       except \"new-line\" and the characters in the string.  For",
-   "       example, \"[xyz]\" matches \"xx\" and \"zyx\", while \"[^xyz]\"",
-   "       matches \"abc\" but not \"axb\".  A range of characters may be",
-   "       specified by two characters separated by \"-\".  Note that,",
-   "       [a-z] matches alphabetics, while [z-a] never matches.",
-   "The concatenation of regular expressions is a regular expression.",
-   0};
-#endif
+char *patdoc[] = {"The regular_expression defines the pattern to search for.  Upper- and",
+                  "lower-case are always ignored.  Blank lines never match.  The expression",
+                  "should be quoted to prevent file-name translation.",
+                  "x      An ordinary character (not mentioned below) matches that character.",
+                  "'\\'    The backslash quotes any character.  \"\\$\" matches a dollar-sign.",
+                  "'^'    A circumflex at the beginning of an expression matches the",
+                  "       beginning of a line.",
+                  "'$'    A dollar-sign at the end of an expression matches the end of a line.",
+                  "'.'    A period matches any character except \"new-line\".",
+                  "':a'   A colon matches a class of characters described by the following",
+                  "':d'     character.  \":a\" matches any alphabetic, \":d\" matches digits,",
+                  "':n'     \":n\" matches alphanumerics, \": \" matches spaces, tabs, and",
+                  "': '     other control characters, such as new-line.",
+                  "'*'    An expression followed by an asterisk matches zero or more",
+                  "       occurrences of that expression: \"fo*\" matches \"f\", \"fo\"",
+                  "       \"foo\", etc.",
+                  "'+'    An expression followed by a plus sign matches one or more",
+                  "       occurrences of that expression: \"fo+\" matches \"fo\", etc.",
+                  "'-'    An expression followed by a minus sign optionally matches",
+                  "       the expression.",
+                  "'[]'   A string enclosed in square brackets matches any character in",
+                  "       that string, but no others.  If the first character in the",
+                  "       string is a circumflex, the expression matches any character",
+                  "       except \"new-line\" and the characters in the string.  For",
+                  "       example, \"[xyz]\" matches \"xx\" and \"zyx\", while \"[^xyz]\"",
+                  "       matches \"abc\" but not \"axb\".  A range of characters may be",
+                  "       specified by two characters separated by \"-\".  Note that,",
+                  "       [a-z] matches alphabetics, while [z-a] never matches.",
+                  "The concatenation of regular expressions is a regular expression.",
+                  0};
 
 #define LMAX 512
 #define PMAX 256
@@ -97,6 +95,10 @@ char *pp, lbuf[LMAX], pbuf[PMAX];
 
 char *cclass();
 char *pmatch();
+void store(int);
+void error(char *);
+void badpat(char *, char *, char *);
+int match(void);
 
 /*** Display a file name *******************************/
 void file(char *s)
@@ -289,7 +291,7 @@ void badpat(char *message, char *source, char *stop)
 /* char  *stop;          // Pattern end   */
 {
     fprintf(stderr, "-GREP-E-%s, pattern is\"%s\"\n", message, source);
-    fprintf(stderr, "-GREP-E-Stopped at byte %d, '%c'\n", stop - source, stop[-1]);
+    fprintf(stderr, "-GREP-E-Stopped at byte %ld, '%c'\n", stop - source, stop[-1]);
     error("?GREP-E-Bad pattern\n");
 }
 
@@ -326,7 +328,7 @@ void grep(FILE *fp, char *fn)
 }
 
 /*** Match line (lbuf) with pattern (pbuf) return 1 if match ***/
-void match()
+int match()
 {
     char *l; /* Line pointer       */
 
@@ -356,7 +358,7 @@ char *pmatch(char *line, char *pattern)
     p = pattern;
     while ((op = *p++) != ENDPAT) {
         if (debug > 1)
-            printf("byte[%d] = 0%o, '%c', op = 0%o\n", l - line, *l, *l, op);
+            printf("byte[%ld] = 0%o, '%c', op = 0%o\n", l - line, *l, *l, op);
         switch (op) {
         case CHAR:
             if (tolower(*l++) != *p++)
