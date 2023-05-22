e43641f09152951c87287dd1198ea5c287af8eae
Henry Kroll III <henry@comptune.com>
2023-05-22 13:35:34 +0800

    tccgen.c: off by one in flexible array members

--- tccgen.c
+++ tccgen.c
@@ -5809,7 +5809,7 @@ static void decl_initializer_alloc(
             tcc_error("unknown type size");
     }
     if (flexible_array)
-        size += flexible_array->type.ref->c * pointed_size(&flexible_array->type);
+        size += flexible_array->type.ref->c * pointed_size(&flexible_array->type) + 1;
     /* take into account specified alignment if bigger */
     if (ad->a.aligned) {
         if (ad->a.aligned > align)
