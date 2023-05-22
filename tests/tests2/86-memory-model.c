6790ded35b9191e7c31239beddb84e1ccde24cd3
Jean-Claude Beaudoin <jean.claude.beaudoin@gmail.com>
2023-05-22 13:54:22 +0800

    Rein in unintended external functions.

--- tests/tests2/86-memory-model.c
+++ tests/tests2/86-memory-model.c
@@ -3,21 +3,21 @@
 int main()
 {
 #if defined(__LLP64__)
-    if (sizeof(short) == 2 sizeof(int) == 4 sizeof(long int) == 4 sizeof(long long int)
-        == 8 sizeof(void *) == 8) {
+    if (sizeof(short) == 2 && sizeof(int) == 4 && sizeof(long int) == 4
+        && sizeof(long long int) == 8 && sizeof(void *) == 8) {
         (void) printf("Ok\n");
     } else {
         (void) printf("KO __LLP64__\n");
     }
 #elif defined(__LP64__)
-    if (sizeof(short) == 2 sizeof(int) == 4 sizeof(long int) == 8 sizeof(long long int)
-        == 8 sizeof(void *) == 8) {
+    if (sizeof(short) == 2 && sizeof(int) == 4 && sizeof(long int) == 8
+        && sizeof(long long int) == 8 && sizeof(void *) == 8) {
         (void) printf("Ok\n");
     } else {
         (void) printf("KO __LP64__\n");
     }
 #elif defined(__ILP32__)
-    if (sizeof(short) == 2 sizeof(int) == 4 sizeof(long int) == 4 sizeof(void *) == 4) {
+    if (sizeof(short) == 2 && sizeof(int) == 4 && sizeof(long int) == 4 && sizeof(void *) == 4) {
         (void) printf("Ok\n");
     } else {
         (void) printf("KO __ILP32__\n");
