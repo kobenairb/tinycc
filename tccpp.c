de25abab6cc21d6ab364e8983fcbadd792296517
seyko <seyko2@gmail.com>
2023-05-22 13:43:14 +0800

    fix preprocessing *.S with ` ' chars in #comments

--- tccpp.c
+++ tccpp.c
@@ -778,6 +778,8 @@ redo_start:
                     in_warn_or_error = 1;
                 else if (tok == TOK_LINEFEED)
                     goto redo_start;
+                else if (parse_flags & PARSE_FLAG_ASM_FILE)
+                    p = parse_line_comment(p);
             } else if (parse_flags & PARSE_FLAG_ASM_FILE)
                 p = parse_line_comment(p);
             break;
@@ -2760,6 +2762,8 @@ redo_no_start:
         p++;
         break;
     default:
+        if (parse_flags & PARSE_FLAG_ASM_FILE)
+            goto parse_simple;
         tcc_error("unrecognized character \\x%02x", c);
         break;
     }
