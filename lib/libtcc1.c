5753009b8708a3dc97f78fe319366e162f16f519
Edmund Grimley Evans <Edmund.Grimley.Evans@gmail.com>
2023-05-22 13:23:15 +0800

    Correct prototype: void __clear_cache(void *, void *).

--- lib/libtcc1.c
+++ lib/libtcc1.c
@@ -686,7 +686,7 @@ void *__va_arg(__va_list_struct *ap, enum __va_arg_type arg_type, int size, int 
 /* Flushing for tccrun */
 #if defined(TCC_TARGET_X86_64) || defined(TCC_TARGET_I386)
 
-void __clear_cache(char *beginning, char *end) {}
+void __clear_cache(void *beginning, void *end) {}
 
 #elif defined(TCC_TARGET_ARM)
 
@@ -695,7 +695,7 @@ void __clear_cache(char *beginning, char *end) {}
 #include <sys/syscall.h>
 #include <stdio.h>
 
-void __clear_cache(char *beginning, char *end)
+void __clear_cache(void *beginning, void *end)
 {
 /* __ARM_NR_cacheflush is kernel private and should not be used in user space.
  * However, there is no ARM asm parser in tcc so we use it for now */
